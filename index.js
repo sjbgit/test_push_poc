51230f54136e968baabe430efeb506b045e9a349
Seth Bunke <seth.bunke@bl-1614.blackline.corp>
2016-08-09 14:58:49 -0700

    cors

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ function generateRandomData() {
 //     console.log();
 // }, 5000);
 
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}); 
+
 app.get('/', function(req, res){
   res.sendfile('./index.html');    
 }); 
